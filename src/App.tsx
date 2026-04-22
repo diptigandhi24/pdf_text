9126a263894b486ef3cf611631659335403a75c0
diptigandhi24 <gandhidipti.s@gmail.com>
2026-04-22 16:21:39 +0530

    adding nutrient condition

--- src/App.tsx
+++ src/App.tsx
@@ -150,7 +150,12 @@ export default function App() {
       });
     })();
 
-    return () => NutrientViewer?.unload(container);
+    // return () => NutrientViewer?.unload(container);
+    return () => {
+      if (NutrientViewer && container) {
+        NutrientViewer.unload(container);
+      }
+    };
   }, [displayUi]);
 
   // if (!user) {
