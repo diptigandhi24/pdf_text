228afaa80b96ff24922f2a46ed3d523d6f531bc3
diptigandhi24 <gandhidipti.s@gmail.com>
2026-04-22 20:56:43 +0530

    preloading pdf

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@ import { useEffect } from "react";
 import PdfViewer from "./components/pdfViewer";
 
 export default function App() {
-  useEffect(() => {
-    let NutrientViewer;
-    console.log("nutrient viewer Before");
-    (async () => {
-      // Lazy load the SDK module early
-      NutrientViewer = await import("@nutrient-sdk/viewer");
+  // useEffect(() => {
+  //   let NutrientViewer;
+  //   console.log("nutrient viewer Before");
+  //   (async () => {
+  //     // Lazy load the SDK module early
+  //     NutrientViewer = await import("@nutrient-sdk/viewer");
 
-      // Then preload the WebAssembly worker in the background
-      NutrientViewer.preloadWorker({
-        baseUrl: `${window.location.protocol}//${window.location.host}/${
-          import.meta.env.PUBLIC_URL ?? ""
-        }`,
-      });
-    })();
-    console.log("nutrient viewer After");
-  }, []);
+  //     // Then preload the WebAssembly worker in the background
+  //     NutrientViewer.preloadWorker({
+  //       baseUrl: `${window.location.protocol}//${window.location.host}/${
+  //         import.meta.env.PUBLIC_URL ?? ""
+  //       }`,
+  //     });
+  //   })();
+  //   console.log("nutrient viewer After");
+  // }, []);
   return <PdfViewer />;
 }
