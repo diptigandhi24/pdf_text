a2de1822a8941c0d288bf2c9e416a83da698554d
diptigandhi24 <gandhidipti.s@gmail.com>
2026-04-22 11:43:41 +0530

    debugging hash

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+console.log("FULL URL:", window.location.href);
+console.log("HASH:", window.location.hash);
+console.log("SEARCH:", window.location.search);
+console.log("PATHNAME:", window.location.pathname);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
